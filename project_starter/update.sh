a037b989066133290db588a92e5425a427503d87
Ehbraheem <ehbraheem@gmail.com>
2022-12-04 03:09:15 +0100

    Completed the high availability project

---
+++ project_starter/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+
+aws cloudformation update-stack \
+  --stack-name $1 \
+  --region us-west-2 \
+  --template-body file://$2 \
+  --parameters file://$3 \
+  --capabilities CAPABILITY_IAM \
+  --profile udacity
